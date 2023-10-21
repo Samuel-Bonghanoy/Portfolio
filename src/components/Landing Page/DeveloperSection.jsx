d3255414a36bbd357ee52671ecde7b47a3d16e02
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 14:32:49 +0800

    feat: configured react router

--- src/components/Landing Page/DeveloperSection.jsx
+++ src/components/Landing Page/DeveloperSection.jsx
@@ -1,7 +1,7 @@
 function DeveloperSection() {
   return (
     <div className="mt-[20%] text-center flex flex-col">
-      <h1 className="text-mainwhite font-black text-5xl">&lt;developer/&gt;</h1>
+      <h1 className="text-mainwhite font-black text-6xl">&lt;developer/&gt;</h1>
       <p className="text-mainwhite">
         Computer Science student and aspiring fullstack web developer
       </p>
--- src/components/Landing Page/PhotographySection.jsx
+++ src/components/Landing Page/PhotographySection.jsx
@@ -1,7 +1,7 @@
 function PhotographySection() {
   return (
     <div className="mt-[20%] text-center flex flex-col">
-      <h1 className="font-black text-5xl">photographer</h1>
+      <h1 className="font-black text-6xl">photographer</h1>
       <p>Street, landscape, and architectural photography hobbyist</p>
     </div>
   );
