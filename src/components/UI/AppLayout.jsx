359425618a3bbca2835a9a03a7510e79d9c2c7d0
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 12:40:33 +0800

    feat: edited navbar component

--- src/components/UI/AppLayout.jsx
+++ src/components/UI/AppLayout.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 function AppLayout() {
   return (
-    <div>
+    <div className="h-screen">
       <Navbar />
     </div>
   );
--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -2,8 +2,12 @@ import devLogo from "../../assets/Dev_logo_red_o.png";
 
 function Navbar() {
   return (
-    <div className="w-screen bg-slate-200">
-      <img src={devLogo} />
+    <div className="w-screen h-[15%] bg-slate-200 flex items-center">
+      <img className="h-[45%] w-[12%] ml-[2%]" src={devLogo} />
+      <div className="flex">
+        <p>about</p>
+        <p>contact</p>
+      </div>
     </div>
   );
 }
