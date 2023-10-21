be850ce726db392bc16a38d574be48311bb3887b
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 21:32:14 +0800

    feat: edited developer section

--- src/components/Landing Page/DeveloperSection.jsx
+++ src/components/Landing Page/DeveloperSection.jsx
@@ -1,20 +1,23 @@
 function DeveloperSection() {
   return (
     <>
-      <div className="pt-[20%] flex flex-col h-[100%] justify-between">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-mainwhite font-black text-6xl mx-auto">
-            &lt;developer/&gt;
+      <div className="pt-[17%] flex flex-col h-[100%] justify-between">
+        <div className="flex flex-col gap-5 mx-auto">
+          <h1 className="text-noto text-4xl font-bold text-mainwhite">
+            デベロッパー
           </h1>
-          <p className="text-gray-300 line-clamp-3 text-clip mx-auto max-w-[45ch]">
+          <h1 className="text-mainwhite font-black text-6xl ">
+            &lt;developer&gt;
+          </h1>
+          <p className="text-gray-300 text-clip mt-2 max-w-[45ch]">
             Computer Science student and aspiring fullstack web developer in the
             Philippines.
           </p>
-        </div>
-        <div className="pb-[8%] self-center justify-self-end">
-          <button className="rounded-lg border-4 text-mainwhite font-bold border-mainwhite border-solid w-[12rem] px-3 py-3 self-center justify-self-end text-lg hover:cursor-pointer duration-100 hover:border-crimson hover:text-crimson">
-            view my portfolio
-          </button>
+          <div className="pt-[10%] justify-self-end">
+            <button className="rounded-lg border-4 border-solid text-maindark  w-[12rem] px-2 py-3 border-mainwhite self-center justify-self-end text-lg font-black bg-mainwhite hover:cursor-pointer duration-100 hover:border-crimson hover:text-crimson">
+              view my portfolio
+            </button>
+          </div>
         </div>
       </div>
     </>
