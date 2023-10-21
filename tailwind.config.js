6c0fa24a71f5af01e9b640b0969cb10da9f661d8
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 21:50:47 +0800

    feat: edited developer section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -35,5 +37,5 @@ export default {
       },
     },
   },
-  plugins: ["@tailwindcss/line-clamp"],
+  plugins: ["@tailwindcss/line-clamp", daisyui],
 };
