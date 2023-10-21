61355622413a179094f03f859c289126707b26b2
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 20:44:15 +0800

    feat: edited developersection

--- src/components/Landing Page/DeveloperSection.jsx
+++ src/components/Landing Page/DeveloperSection.jsx
@@ -1,11 +1,22 @@
 function DeveloperSection() {
   return (
-    <div className="mt-[20%] text-center flex flex-col">
-      <h1 className="text-mainwhite font-black text-6xl">&lt;developer/&gt;</h1>
-      <p className="text-gray-300 mt-3 line-clamp-3 text-clip ">
-        Computer Science student and aspiring fullstack web developer
-      </p>
-    </div>
+    <>
+      <div className="pt-[20%] text-center flex flex-col h-[100%] justify-between">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-mainwhite font-black text-6xl">
+            &lt;developer/&gt;
+          </h1>
+          <p className="text-gray-300  line-clamp-3 text-clip ">
+            Computer Science student and aspiring fullstack web developer
+          </p>
+        </div>
+        <div className="pb-[15%] self-center justify-self-end">
+          <button className="rounded-lg border-4 text-mainwhite font-bold border-mainwhite border-solid w-[12rem] px-3 py-3 self-center justify-self-end text-lg ">
+            view my portfolio
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Landing Page/PhotographySection.jsx
+++ src/components/Landing Page/PhotographySection.jsx
@@ -1,15 +1,15 @@
 function PhotographySection() {
   return (
     <>
-      <div className="pt-[20%] text-darktext flex flex-col h-[100%]  justify-between">
+      <div className="pt-[20%] text-darktext flex flex-col h-[100%] justify-between">
         <div className="flex flex-col gap-4">
           <h1 className="font-black text-6xl mx-auto">photographer</h1>
-          <p className="text-md  mx-auto">
+          <p className="text-md mx-auto">
             Street, landscape, and architectural photography hobbyist
           </p>
         </div>
         <div className="pb-[15%] self-center justify-self-end">
-          <button className="rounded-lg border-4 border-solid  w-[12rem] px-3 py-3 border-maindark self-center justify-self-end text-lg font-black">
+          <button className="rounded-lg border-4 border-solid  w-[12rem] px-3 py-3 border-maindark self-center justify-self-end text-lg font-bold">
             view my gallery
           </button>
         </div>
