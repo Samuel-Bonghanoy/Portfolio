9858017a3f48df6aeaa8f29bdbf8f4f0752051d8
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 11:38:14 +0800

    feat: initialized repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Circles from "./components/Cursor Trail/Circles";
+
+function App() {
+  return (
+    <div>
+      <Circles />
+      <div className="bg-slate-300">
+        <h1>HI</h1>
+        <button>yes</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
