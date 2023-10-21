c96199b3e636f828f02811bf44e418e2768eb55f
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 15:39:56 +0800

    feat: configured react router

--- src/components/Landing Page/PhotographySection.jsx
+++ src/components/Landing Page/PhotographySection.jsx
@@ -1,11 +1,18 @@
+import { Button } from "@mui/material";
+
 function PhotographySection() {
   return (
-    <div className="mt-[20%] text-darktext flex flex-col  ">
-      <h1 className="font-black text-6xl mx-auto">photographer</h1>
-      <p className="text-md mt-3  mx-auto">
-        Street, landscape, and architectural photography hobbyist
-      </p>
-    </div>
+    <>
+      <div className="mt-[20%] text-darktext flex flex-col  ">
+        <h1 className="font-black text-6xl mx-auto">photographer</h1>
+        <p className="text-md mt-3  mx-auto">
+          Street, landscape, and architectural photography hobbyist
+        </p>
+        <button className="rounded border-solid  w-[10rem] px-3 py-3 border-maindark self-center">
+          View my gallery
+        </button>
+      </div>
+    </>
   );
 }
 
