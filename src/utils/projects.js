a3a0b34a931bce535cbd83ab8575bb2d4ae5e5d1
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 23:55:20 +0800

    fix: removed horizontal scrollbar

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,8 +1,25 @@
 const projects = [
   {
     title: "Cabinscape",
+    imageUrl: "cabinscape.png",
+    description: "A cabin reservation app for vacations and leisure",
+    tagOne: "React",
+    tagTwo: "Supabase",
   },
-  {},
+  {
+    title: "Natoura",
+    imageUrl: "natoura.png",
+    description: "A tour booking and tracking application",
+    tagOne: "ExpressJS",
+    tagTwo: "Pug",
+  },
+  {
+    title: "Kitsune",
+    imageUrl: "kitsune.png",
+    description: "A \full-stack blog app influenced by X",
+    tagOne: "ExpressJS",
+    tagTwo: "React",
+  },
 ];
 
 export default projects;
