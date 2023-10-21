fcd9b12192772f7e4d28c12ff2c5a7461a3cc031
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 21:26:40 +0800

    feat: edited tailwind config

--- src/components/Landing Page/PhotographySection.jsx
+++ src/components/Landing Page/PhotographySection.jsx
@@ -9,8 +9,8 @@ function PhotographySection() {
             Recreational photography hobbyist whose subjects are mostly street,
             landscape, and architectural.
           </p>
-          <div className="pb-[8%] justify-self-end">
-            <button className="rounded-lg border-4 border-solid  w-[12rem] px-3 py-3 border-maindark self-center justify-self-end text-lg font-bold hover:cursor-pointer duration-100 hover:border-crimson hover:text-crimson">
+          <div className="pt-[10%] justify-self-end">
+            <button className="rounded-lg border-4 border-solid text-mainwhite  w-[12rem] px-2 py-3 border-maindark self-center justify-self-end text-lg font-black bg-maindark hover:cursor-pointer duration-100 hover:border-crimson hover:text-crimson">
               view my gallery
             </button>
           </div>
