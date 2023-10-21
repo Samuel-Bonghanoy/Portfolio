fd305a32fe30367e8b092d631d56b7e1ea264585
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-22 00:04:54 +0800

    fix: removed horizontal scrollbar

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -11,8 +11,8 @@ function Card({ project }) {
         </h2>
         <p>{project.description}</p>
         <div className="card-actions justify-end">
-          <div className="badge badge-outline">{project.tagOne}</div>
-          <div className="badge badge-outline">{project.tagTwo}</div>
+          <div className="badge badge-primary">{project.tagOne}</div>
+          <div className="badge badge-accent">{project.tagTwo}</div>
         </div>
       </div>
     </div>
