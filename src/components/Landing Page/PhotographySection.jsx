29bb6d5a1a82c19203ec6c687a6b0230c59eaac6
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 15:46:44 +0800

    feat: configured react router

--- src/components/Landing Page/PhotographySection.jsx
+++ src/components/Landing Page/PhotographySection.jsx
@@ -3,14 +3,18 @@ import { Button } from "@mui/material";
 function PhotographySection() {
   return (
     <>
-      <div className="mt-[20%] text-darktext flex flex-col  ">
-        <h1 className="font-black text-6xl mx-auto">photographer</h1>
-        <p className="text-md mt-3  mx-auto">
-          Street, landscape, and architectural photography hobbyist
-        </p>
-        <button className="rounded border-solid  w-[10rem] px-3 py-3 border-maindark self-center">
-          View my gallery
-        </button>
+      <div className="pt-[20%] text-darktext flex flex-col h-[100%]  justify-between">
+        <div className="flex flex-col gap-4">
+          <h1 className="font-black text-6xl mx-auto">photographer</h1>
+          <p className="text-md  mx-auto">
+            Street, landscape, and architectural photography hobbyist
+          </p>
+        </div>
+        <div className="pb-[15%] self-center justify-self-end">
+          <button className="rounded-lg border border-4 border-solid  w-[12rem] px-3 py-3 border-maindark self-center justify-self-end text-lg font-black">
+            View my gallery
+          </button>
+        </div>
       </div>
     </>
   );
