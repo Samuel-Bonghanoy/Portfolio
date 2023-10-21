2a9063febd439ce695cd16e2fb544a9bde226d52
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 13:01:07 +0800

    feat: configured react router

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,12 +1,20 @@
 import devLogo from "../../assets/Dev_logo_red_o.png";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Navbar() {
   return (
     <div className="w-screen h-[15%] bg-slate-200 flex items-center justify-between">
-      <img className="h-[45%] w-[12%] ml-[2%]" src={devLogo} />
-      <div className="flex gap-[15%]">
-        <p>about</p>
-        <p>contact</p>
+      <img className="h-[40%] w-[10%] ml-[2%]" src={devLogo} />
+      <div className="flex gap-20 items-center mr-[2%] font-raleway font-semibold w-[20%]">
+        <div className="flex gap-8">
+          <p>about</p>
+          <p>contact</p>
+        </div>
+        <div className="flex gap-5">
+          <LinkedInIcon style={{ width: "2rem", height: "2rem" }} />
+          <GitHubIcon style={{ width: "2rem", height: "2rem" }} />
+        </div>
       </div>
     </div>
   );
