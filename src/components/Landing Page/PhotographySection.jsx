2bc66dabeee027c9c8b33839e9cd078b7dabc431
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-21 21:23:19 +0800

    feat: edited tailwind config

--- src/components/Landing Page/PhotographySection.jsx
+++ src/components/Landing Page/PhotographySection.jsx
@@ -1,18 +1,19 @@
 function PhotographySection() {
   return (
     <>
-      <div className="pt-[20%] text-darktext flex flex-col h-[100%] justify-between">
-        <div className="flex flex-col gap-4">
-          <h1 className="font-black text-6xl mx-auto">photographer</h1>
-          <p className="text-md mx-auto max-w-[47ch]">
+      <div className="pt-[17%] text-darktext flex flex-col h-[100%] justify-between">
+        <div className="flex flex-col gap-5 mx-auto">
+          <h1 className="text-noto text-4xl font-bold">フォトグラファー</h1>
+          <h1 className="font-black text-6xl ">photographer</h1>
+          <p className="text-md mt-2 max-w-[47ch]">
             Recreational photography hobbyist whose subjects are mostly street,
             landscape, and architectural.
           </p>
-        </div>
-        <div className="pb-[8%] self-center justify-self-end">
-          <button className="rounded-lg border-4 border-solid  w-[12rem] px-3 py-3 border-maindark self-center justify-self-end text-lg font-bold hover:cursor-pointer duration-100 hover:border-crimson hover:text-crimson">
-            view my gallery
-          </button>
+          <div className="pb-[8%] justify-self-end">
+            <button className="rounded-lg border-4 border-solid  w-[12rem] px-3 py-3 border-maindark self-center justify-self-end text-lg font-bold hover:cursor-pointer duration-100 hover:border-crimson hover:text-crimson">
+              view my gallery
+            </button>
+          </div>
         </div>
       </div>
     </>
