95311523580806a3633d986abfbe253812bf7f43
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-23 02:20:22 +0800

    fix: removed horizontal scrollbar

--- src/components/About Page/Header.jsx
+++ src/components/About Page/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <div className="pt-[15%] text-darktext flex flex-col h-[100%] shadow-mainwhite shadow-lg">
-        <div className="flex flex-col gap-5 font-bold ml-[10rem]">
+        <div className="flex flex-col gap-5 font-bold mx-auto">
           <h1 className="text-noto text-6xl font-bold">バックグラウンド</h1>
           <h1 className="font-black text-8xl mb-3">about.</h1>
           <p className="text-lg text-graytext mt-2 max-w-[64ch] font-normal">
