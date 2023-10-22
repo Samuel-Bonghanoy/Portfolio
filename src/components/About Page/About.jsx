d298d5d8cc6018c68cba7839f9dcb21772ee8c00
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-23 00:29:56 +0800

    feat: configured react router in navbar

--- src/components/About Page/About.jsx
+++ src/components/About Page/About.jsx
@@ -1,13 +1,14 @@
 import DeveloperSection from "../Landing Page/DeveloperSection";
-import PhotographySection from "../Landing Page/PhotographySection";
+
+import Header from "./Header";
 
 function About() {
   return (
     <>
       <div className="overflow-x-hidden shadow-maindark">
-        <div className="h-[75vh] w-screen grid grid-cols-2 overflow-x-hidden overflow-y-hidden shadow-maindark shadow-lg">
+        <div className="h-[100vh] w-screen grid grid-cols-2 overflow-x-hidden overflow-y-hidden shadow-maindark shadow-lg">
           <div className="bg-mainwhite  transition-all duration-500 z-50  shadow-maindark shadow-lg">
-            <PhotographySection />
+            <Header />
           </div>
           <div className="bg-maindark transition-all duration-500 z-50 shadow-maindark shadow-lg">
             <DeveloperSection />
