46ed9d9b19cd3fcd7e609dd4e902e7e8f8a9ae1c
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-25 00:55:18 +0800

    fix: fixed navbar navigation bug

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -6,8 +6,8 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div className="w-full h-[12%] z-[70]  flex items-center justify-between bg-mainwhite backdrop-blur-[2px] bg-opacity-5 absolute overflow-x-hidden">
-      <NavLink className="h-full w-full ml-[2%] flex items-center" to="/">
-        <img className="h-[50%] w-[13%] " src={devLogo} />
+      <NavLink className="ml-[2%] flex items-center" to="/">
+        <img className="h-[57%] w-[17%] " src={devLogo} />
       </NavLink>
       <div className="flex gap-20 items-center mr-[2%] font-raleway font-semibold w-[20%]">
         <div className="flex gap-8">
