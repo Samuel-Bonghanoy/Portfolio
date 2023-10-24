94886e03206891bcc889dfffb99baef865f96c55
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-24 23:45:23 +0800

    configured framer for animations on page transfer

--- src/components/Contact Page/ContactMethods.jsx
+++ src/components/Contact Page/ContactMethods.jsx
@@ -1,9 +1,16 @@
 import Form from "./Form";
+import SocialMedia from "./SocialMedia";
 
 function ContactMethods() {
   return (
     <div className="pt-[13%] h-full">
       <Form />
+      <div className="mt-3 flex w-[80%] mx-auto flex-col border-white">
+        <div className="divider text-mainwhitefont font-semibold before:bg-mainwhite after:bg-mainwhite ">
+          Or contact me via social media
+        </div>
+      </div>
+      <SocialMedia />
     </div>
   );
 }
--- src/components/Contact Page/Form.jsx
+++ src/components/Contact Page/Form.jsx
@@ -76,11 +76,6 @@ function Form() {
           Submit
         </button>
       </form>
-      <div className="mt-3 flex w-[80%] mx-auto flex-col border-white">
-        <div className="divider text-mainwhitefont font-semibold before:bg-mainwhite after:bg-mainwhite ">
-          Or contact me via social media
-        </div>
-      </div>
     </>
   );
 }
