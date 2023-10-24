0e1f83cfcc8b707225962a2b52c0678448ecfbab
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-24 23:49:55 +0800

    configured framer for animations on page transfer

--- src/components/Contact Page/Contact.jsx
+++ src/components/Contact Page/Contact.jsx
@@ -1,34 +1,36 @@
 import ContactHeader from "./ContactHeader";
 import ContactMethods from "./ContactMethods";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Contact() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 1.5 } }}
-      exit={{ opacity: 0 }}
-      className="overflow-x-hidden shadow-maindark"
-    >
-      <div className="h-[100vh] w-screen grid grid-cols-[0.40fr_0.60fr] overflow-x-hidden overflow-y-hidden shadow-maindark shadow-lg">
-        <motion.div
-          initial={{ opacity: 0, x: -150 }}
-          animate={{ opacity: 1, x: 0, transition: { duration: 0.1 } }}
-          exit={{ opacity: 0, x: -50 }}
-          className="bg-mainwhite  transition-all duration-500 z-50  shadow-maindark shadow-lg hover:scale-[1.01] hover:scale-x-[1.02] hover:z-[60]"
-        >
-          <ContactHeader />
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, x: 200 }}
-          animate={{ opacity: 1, x: 0, transition: { duration: 0.1 } }}
-          exit={{ opacity: 0, x: 100 }}
-          className="bg-maindark h-full transition-all duration-500 z-50 shadow-maindark shadow-lg sticky"
-        >
-          <ContactMethods />
-        </motion.div>
-      </div>
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 1.5 } }}
+        exit={{ opacity: 0 }}
+        className="overflow-x-hidden shadow-maindark"
+      >
+        <div className="h-[100vh] w-screen grid grid-cols-[0.40fr_0.60fr] overflow-x-hidden overflow-y-hidden shadow-maindark shadow-lg">
+          <motion.div
+            initial={{ opacity: 0, x: -150 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 0.1 } }}
+            exit={{ opacity: 0, x: -50 }}
+            className="bg-mainwhite  transition-all duration-500 z-50  shadow-maindark shadow-lg hover:scale-[1.01] hover:scale-x-[1.02] hover:z-[60]"
+          >
+            <ContactHeader />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 0.1 } }}
+            exit={{ opacity: 0, x: 100 }}
+            className="bg-maindark h-full transition-all duration-500 z-50 shadow-maindark shadow-lg sticky"
+          >
+            <ContactMethods />
+          </motion.div>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
