b0918055a3b9a9371664abe95b6b9c2c2d704913
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-24 22:03:04 +0800

    feat: install formspree

--- src/components/Contact Page/Form.jsx
+++ src/components/Contact Page/Form.jsx
@@ -1,27 +1,52 @@
 import { useForm, ValidationError } from "@formspree/react";
+import { useState } from "react";
 
 function Form() {
   const [state, handleSubmit] = useForm("xdorbdze");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   return (
-    <form onSubmit={handleSubmit} className="mx-auto w-fit flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="mx-auto w-fit flex  gap-3">
       <div className="flex flex-col">
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="email" />
+        <div className="flex flex-col">
+          <label htmlFor="email">Email Address</label>
+          <input
+            aria-label="Email"
+            id="email"
+            type="email"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="name">Full Name</label>
+          <input
+            aria-label="Name"
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
       </div>
 
       <div className="flex flex-col">
-        <input
-          aria-label="Name"
-          type="text"
-          name="name"
-          value={name}
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <label htmlFor="Message">Message</label>
+        <textarea
+          aria-label="Message"
+          id="message"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           required
         />
-      </div>
-      <div>
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <textarea id="message" name="message" />
         <ValidationError
           prefix="Message"
           field="message"
