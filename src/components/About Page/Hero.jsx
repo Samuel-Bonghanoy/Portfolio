297779516343939665398c87e442387afc95dfe8
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-25 00:58:09 +0800

    fix: removed horizontal scrollbar

--- src/components/About Page/Hero.jsx
+++ src/components/About Page/Hero.jsx
@@ -2,7 +2,7 @@ function DeveloperSection() {
   return (
     <>
       <div className="pt-[2%] flex flex-col w-full h-[100%] items-center justify-center shadow-maindark shadow-xl sticky">
-        <img src="/samuel.png" className="h-[80%]" />
+        <img src="/samuel.png" loading="lazy" className="h-[80%]" />
       </div>
     </>
   );
