25a03c281e9b0dda7821d5a6ed7effcb037b2c82
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-25 12:54:51 +0800

    feat: refactored navbar component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,10 +1,39 @@
+import GalleryHeader from "./GalleryHeader";
 import GalleryNav from "./GalleryNav";
 import GalleryPictures from "./GalleryPictures";
+import { motion } from "framer-motion";
 
 function Gallery() {
   return (
     <>
       <GalleryNav />
+      <motion.div
+        // initial={{ opacity: 0 }}
+        // animate={{ opacity: 1, transition: { duration: 1.2 } }}
+        // exit={{ opacity: 0 }}
+        className="overflow-x-hidden shadow-maindark"
+      >
+        <div className="h-[100vh] w-screen grid grid-cols-[0.6fr_0.40fr] overflow-x-hidden overflow-y-hidden shadow-maindark shadow-lg">
+          <motion.div
+            initial={{ opacity: 1, x: -200 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 0.1 } }}
+            exit={{ opacity: 1, x: -200 }}
+            whileHover={{
+              scale: 1.03,
+              zIndex: 60,
+              transition: { duration: 0.05 },
+            }}
+            className="bg-mainwhite  transition-all duration-500 z-50  shadow-maindark shadow-lg hover:scale-[1.01] hover:scale-x-[1.02] "
+          >
+            <GalleryHeader />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 1, x: 150 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 0.1 } }}
+            className="bg-maindark transition-all duration-500 z-50 shadow-maindark shadow-lg sticky"
+          ></motion.div>
+        </div>
+      </motion.div>
       <GalleryPictures />
     </>
   );
--- src/components/Gallery/GalleryHeader.jsx
+++ src/components/Gallery/GalleryHeader.jsx
@@ -1,5 +1,37 @@
+import { motion } from "framer-motion";
+
 function GalleryHeader() {
-  return <div></div>;
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0, x: -150 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+          transition: { duration: 0.5, delay: 0.3 },
+        }}
+        exit={{ opacity: 0, x: -150 }}
+        className="pt-[30%] text-darktext flex flex-col h-[100%] shadow-mainwhite shadow-lg"
+      >
+        <div className="flex flex-col gap-5 font-bold mx-auto">
+          <h1 className="text-noto text-6xl font-bold">れんらく</h1>
+          <h1 className="font-black text-8xl mb-3">contact.</h1>
+          <motion.p
+            initial={{ opacity: 0, x: -150 }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 0.5, delay: 0.5 },
+            }}
+            className="text-lg text-gray-600 mt-2 max-w-[45ch] font-normal"
+          >
+            Feel free to get in touch with me through any of my social media
+            accounts or via email
+          </motion.p>
+        </div>
+      </motion.div>
+    </>
+  );
 }
 
 export default GalleryHeader;
