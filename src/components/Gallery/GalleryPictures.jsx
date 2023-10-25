3c3dd53222300b53161ecc4f9d17a7d587edc788
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-25 23:02:13 +0800

    feat: added gallery pics

--- src/components/Gallery/GalleryPictures.jsx
+++ src/components/Gallery/GalleryPictures.jsx
@@ -9,7 +9,9 @@ import Tower from "/Tower2.jpg";
 import docks from "/docks.jpg";
 import hp2 from "/hp2.jpg";
 import hp3 from "/hp3.jpg";
-import spicy from "/spicy.jpg";
+import spicy from "/duck.jpg";
+
+import { motion } from "framer-motion";
 
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
@@ -19,115 +21,194 @@ function GalleryPictures() {
     <div className="bg-subwhite  border-t-[0.15rem] border-textgray">
       <div className="grid w-[80%] mx-auto py-[4%] grid-cols-2 md:grid-cols-5 gap-4">
         {/* 1 */}
-        <div className="grid grid-rows-[0.4fr_0.25fr_0.35fr] gap-4">
-          <div>
+        <div className="flex flex-col gap-4">
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
-              className="h-full max-w-full rounded-lg"
+              className="h-full max-w-full rounded-lg  transition-all duration-500 hover:scale-[1.03]"
               src="/hamilton.jpg"
               effect="blur"
               alt=""
             />
-          </div>
-          <div>
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
               className="h-[14rem] w-auto max-w-full rounded-lg object-fit"
               src="/night2.jpg"
               effect="blur"
               alt=""
             />
-          </div>
-          <div>
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
               className="h-auto max-w-full rounded-lg"
               src="/silhouette.jpg"
               effect="blur"
               alt=""
             />
-          </div>
+          </motion.div>
         </div>
         {/* 2 */}
-        <div className="flex flex-col gap-4 justify-evenly">
-          <div className="h-fit">
+        <div className="flex flex-col gap-4 ">
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
               effect="blur"
               src={hp2}
               className="h-full rounded-lg"
             ></LazyLoadImage>
-          </div>
-          <div className="h-fit">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
               src={city}
               className="h-full w-full rounded-lg"
             ></LazyLoadImage>
-          </div>
-          <div className="h-[20%]">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
-              src={night1}
+              src={bridge}
               className="h-full rounded-lg"
             ></LazyLoadImage>
-          </div>
+          </motion.div>
         </div>
         {/* 3 */}
         <div className="flex flex-col gap-4">
-          <div className="h-[18%] rounded-lg">
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
               src={docks}
               className="h-full rounded-lg"
             ></LazyLoadImage>
-          </div>
-          <div className="h-[47%] w-full rounded-lg">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+            className="h-[38%]"
+          >
             <LazyLoadImage
               src={crow}
               className="h-full rounded-lg"
             ></LazyLoadImage>
-          </div>
-          <div className="h-[38%] rounded-lg">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+            className="h-[30%]"
+          >
             <LazyLoadImage
               src={Tower}
               className="h-full rounded-lg"
             ></LazyLoadImage>
-          </div>
+          </motion.div>
         </div>
         {/* 4 */}
         <div className="flex flex-col gap-4">
-          <div className="h-[35%] rounded-lg">
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+            className="h-[45%]"
+          >
             <LazyLoadImage
               src={grass}
               className="h-full rounded-lg"
             ></LazyLoadImage>
-          </div>
-          <div className="h-[30%] w-full rounded-lg object-fit">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+            className="h-[23%]"
+          >
             <LazyLoadImage
               src={clock}
-              className="h-full w-full object-cover rounded-lg"
+              className="h-full w-full object-cover rounded-lg object-[85%,15%]"
             ></LazyLoadImage>
-          </div>
-          <div className="h-[35%] w-full rounded-lg">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
               src={hp1}
               className="h-full w-full rounded-lg object-contain"
             ></LazyLoadImage>
-          </div>
+          </motion.div>
         </div>
         <div className="flex flex-col gap-4">
-          <div className="h-[45%] w-full rounded-lg">
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
               src={hp3}
               className="h-full w-full rounded-lg object-contain"
             ></LazyLoadImage>
-          </div>
-          <div className="h-[25%] w-full rounded-lg">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
-              src={clock}
+              src={spicy}
               className="h-full w-full rounded-lg object-contain"
             ></LazyLoadImage>
-          </div>
-          <div className="h-[30%] rounded-lg ">
+          </motion.div>
+          <motion.div
+            whileHover={{
+              scale: 1.02,
+              transition: { duration: 0.1 },
+            }}
+          >
             <LazyLoadImage
-              src={bridge}
+              src={night1}
               className="h-full w-full rounded-lg object-contain"
             ></LazyLoadImage>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
