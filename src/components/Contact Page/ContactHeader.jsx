57d7404645aa0a9eb979d6f89883a4066cd83369
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-25 12:40:27 +0800

    feat: finished gallery section

--- src/components/Contact Page/ContactHeader.jsx
+++ src/components/Contact Page/ContactHeader.jsx
@@ -16,10 +16,18 @@ function ContactHeader() {
         <div className="flex flex-col gap-5 font-bold mx-auto">
           <h1 className="text-noto text-6xl font-bold">れんらく</h1>
           <h1 className="font-black text-8xl mb-3">contact.</h1>
-          <p className="text-lg text-gray-600 mt-2 max-w-[45ch] font-normal">
+          <motion.p
+            initial={{ opacity: 0, x: -150 }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 0.5, delay: 0.5 },
+            }}
+            className="text-lg text-gray-600 mt-2 max-w-[45ch] font-normal"
+          >
             Feel free to get in touch with me through any of my social media
             accounts or via email
-          </p>
+          </motion.p>
         </div>
       </motion.div>
     </>
