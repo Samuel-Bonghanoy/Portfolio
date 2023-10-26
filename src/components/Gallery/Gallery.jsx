5706aa5e1104f6d899b147ceb4e6c3705b1beb59
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-26 11:34:44 +0800

    feat: made styles responsive

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -14,7 +14,7 @@ function Gallery() {
         // exit={{ opacity: 0 }}
         className="overflow-x-hidden shadow-maindark shadow-lg "
       >
-        <div className="h-[80vh] w-screen grid  overflow-x-hidden overflow-y-hidden shadow-maindark shadow-stone-400 border-maindark shadow-lg z-[60]">
+        <div className="sm:h-[60vh] md:h-[80vh] w-screen grid  overflow-x-hidden overflow-y-hidden shadow-maindark shadow-stone-400 border-maindark shadow-lg z-[60]">
           <motion.div className="bg-mainwhite  transition-all duration-500 z-50 border-maindark shadow-stone-400 shadow-lg hover:scale-[1.01] hover:scale-x-[1.02] ">
             <GalleryHeader />
           </motion.div>
--- src/components/Gallery/GalleryHeader.jsx
+++ src/components/Gallery/GalleryHeader.jsx
@@ -11,11 +11,13 @@ function GalleryHeader() {
           transition: { duration: 0.5, delay: 0.3 },
         }}
         exit={{ opacity: 0, x: -150 }}
-        className="pt-[10%] text-darktext flex flex-col h-[100%] shadow-mainwhite shadow-lg"
+        className="sm:pt-[25%] md:pt-[10%] text-darktext flex flex-col h-[100%] shadow-mainwhite shadow-lg"
       >
         <div className="flex flex-col gap-5 font-bold mx-auto">
-          <h1 className="text-noto text-6xl font-bold">ギャラリ</h1>
-          <h1 className="font-black text-8xl mb-3">gallery.</h1>
+          <h1 className="text-noto sm:text-4xl md:text-6xl font-bold">
+            ギャラリ
+          </h1>
+          <h1 className="font-black sm:text-7xl md:text-8xl mb-3">gallery.</h1>
           <motion.p
             initial={{ opacity: 0, x: -150 }}
             animate={{
@@ -23,7 +25,7 @@ function GalleryHeader() {
               x: 0,
               transition: { duration: 0.5, delay: 0.5 },
             }}
-            className="text-lg text-gray-600 mt-2 max-w-[45ch] font-normal"
+            className="sm:text-sm md:text-lg text-gray-600 mt-2 max-w-[45ch] font-normal"
           >
             One of my biggest hobbies is capturing landscapes, streets, and
             buildings with a camera, most especially when I travel. Here are
