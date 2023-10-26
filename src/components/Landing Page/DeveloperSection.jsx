a216338d5405dcbbb3565623485347131165834f
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-26 11:14:06 +0800

    feat: made styles responsive

--- src/components/Landing Page/DeveloperSection.jsx
+++ src/components/Landing Page/DeveloperSection.jsx
@@ -6,11 +6,13 @@ function DeveloperSection() {
     <>
       <div className="pt-[17%] flex flex-col w-full h-[100%]justify-between shadow-maindark shadow-xl">
         <div className="flex flex-col gap-5 mx-auto">
-          <h1 className="text-noto text-4xl font-bold text-mainwhite">
+          <h1 className="text-noto sm:text-2xl md:text-4xl font-bold text-mainwhite">
             かいはつしゃ
           </h1>
-          <h1 className="text-mainwhite font-black text-6xl">developer.</h1>
-          <p className="text-gray-300 text-clip mt-2 max-w-[45ch] font-normal">
+          <h1 className="text-mainwhite font-black sm:text-4xl md:text-6xl">
+            developer.
+          </h1>
+          <p className="text-gray-300 text-clip mt-2 md:max-w-[45ch] sm:max-w-[37ch] sm:text-sm md:text-[1rem] font-normal">
             Computer Science student and aspiring fullstack web developer based
             in the Philippines.
           </p>
@@ -25,7 +27,7 @@ function DeveloperSection() {
               <motion.button
                 whileHover={{ scale: 1.1 }}
                 transition={{ type: "spring", stiffness: 500 }}
-                className="rounded-lg border-4 border-solid text-maindark w-[12rem] px-2 py-3 shadow-mainwhite shadow-sm border-mainwhite self-center justify-self-end text-lg font-bold bg-mainwhite hover:cursor-pointer duration-100 hover:shadow-mainwhite "
+                className="rounded-lg border-4 border-solid text-maindark sm:w-[9rem] md:w-[12rem] px-2 py-3 shadow-mainwhite shadow-sm border-mainwhite self-center justify-self-end sm:text-sm md:text-lg font-bold bg-mainwhite hover:cursor-pointer duration-100 hover:shadow-mainwhite "
               >
                 download my cv
               </motion.button>
--- src/components/Landing Page/PhotographySection.jsx
+++ src/components/Landing Page/PhotographySection.jsx
@@ -4,11 +4,13 @@ import { NavLink } from "react-router-dom";
 function PhotographySection() {
   return (
     <>
-      <div className="pt-[17%] text-darktext flex flex-col h-[100%] justify-between shadow-mainwhite shadow-lg">
+      <div className="sm:pt-[22%] md:pt-[17%] text-darktext flex flex-col h-[100%] justify-between shadow-mainwhite shadow-lg">
         <div className="flex flex-col gap-5 font-bold mx-auto">
-          <h1 className="text-noto text-4xl font-bold">フォトグラファー</h1>
-          <h1 className="font-black text-6xl ">photographer.</h1>
-          <p className="text-md text-graytext mt-2 max-w-[47ch] font-normal">
+          <h1 className="text-noto sm:text-2xl md:text-4xl font-bold">
+            フォトグラファー
+          </h1>
+          <h1 className="font-black sm:text-4xl md:text-6xl ">photographer.</h1>
+          <p className="text-md text-graytext mt-2 md:max-w-[45ch] sm:max-w-[37ch] sm:text-sm md:text-[1rem] font-normal">
             Recreational photography hobbyist whose subjects are mostly street,
             landscape, and architectural.
           </p>
@@ -17,7 +19,7 @@ function PhotographySection() {
               <motion.button
                 whileHover={{ scale: 1.1 }}
                 transition={{ type: "spring", stiffness: 500 }}
-                className="rounded-lg border-4 shadow-maindark shadow-sm border-solid text-mainwhite w-[12rem] px-2 py-3 border-maindark self-center justify-self-end text-lg font-bold bg-maindark hover:cursor-pointer duration-100 hover:shadow-slate-800 hover:scale-[1.05]"
+                className="rounded-lg border-4 shadow-maindark shadow-sm border-solid text-mainwhite sm:w-[9rem] md:w-[12rem] px-2 py-3 border-maindark self-center justify-self-end sm:text-sm md:text-lg font-bold bg-maindark hover:cursor-pointer duration-100 hover:shadow-slate-800 hover:scale-[1.05]"
               >
                 view my gallery
               </motion.button>
