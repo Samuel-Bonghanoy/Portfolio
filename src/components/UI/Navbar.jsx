22732cf02133f9a0b6478ceb77f9149d0f908357
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-26 10:54:07 +0800

    feat: made styles responsive

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -10,61 +10,66 @@ function Navbar() {
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0, transition: { duration: 0.75 } }}
       exit={{ opacity: 0, x: -150 }}
-      className="w-full h-[12%] z-[70]  flex items-center justify-between bg-mainwhite backdrop-blur-[2px] bg-opacity-5 absolute overflow-x-hidden"
+      className="w-full h-[12%] z-[70] flex items-center md:justify-between bg-mainwhite backdrop-blur-[2px] bg-opacity-5 absolute overflow-x-hidden"
     >
       <NavLink className="ml-[2%] flex items-center" to="/">
-        <img className="h-[57%] sm:w-[40%] md:w-[17%] " src={devLogo} />
+        <img className="h-[57%] sm:w-[48%] md:w-[17%] " src={devLogo} />
       </NavLink>
-      <div className="flex gap-20 items-center mr-[2%] sm:text-xs font-raleway font-semibold w-[25%]">
-        <div className="flex gap-8">
-          <NavLink
-            className={({ isActive, isPending, isTransitioning }) =>
-              [
-                isPending ? "pending text-mainwhite" : "",
-                isActive ? "active text-crimson" : "text-mainwhite",
-                isTransitioning ? "transitioning text-mainwhite" : "",
-              ].join(" ")
-            }
-            to="/about"
-          >
-            <p className="duration-100 hover:cursor-pointer hover:text-crimson">
-              about
-            </p>
-          </NavLink>
-          <NavLink
-            to="/contact"
-            className={({ isActive, isPending, isTransitioning }) =>
-              [
-                isPending ? "pending text-mainwhite" : "",
-                isActive ? "active text-crimson" : "text-mainwhite",
-                isTransitioning ? "transitioning text-mainwhite" : "",
-              ].join(" ")
-            }
-          >
-            <p className="sm:text-maindark duration-100 hover:cursor-pointer hover:text-crimson">
-              contact
-            </p>
-          </NavLink>
-
-          <NavLink
-            className={({ isActive, isPending, isTransitioning }) =>
-              [
-                isPending ? "pending text-mainwhite" : "",
-                isActive ? "active text-crimson" : "text-mainwhite",
-                isTransitioning ? "transitioning text-mainwhite" : "",
-              ].join(" ")
-            }
-            to="/gallery"
-          >
-            <p className="duration-100 hover:cursor-pointer hover:text-crimson">
-              gallery
-            </p>
-          </NavLink>
-        </div>
+      <div className="flex sm:gap-10 md:gap-20 items-center sm:mr-0 md:mr-[2%] sm:text-xs font-raleway font-semibold sm:w-[45%] md:w-[25%]">
+        <ul className=" flex sm:flex md:flex-row sm:gap-5 md:gap-8">
+          <li>
+            <NavLink
+              className={({ isActive, isPending, isTransitioning }) =>
+                [
+                  isPending ? "pending text-mainwhite" : "",
+                  isActive ? "active text-crimson" : "text-mainwhite",
+                  isTransitioning ? "transitioning text-mainwhite" : "",
+                ].join(" ")
+              }
+              to="/about"
+            >
+              <p className="sm:text-maindark duration-100 hover:cursor-pointer hover:text-crimson">
+                about
+              </p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive, isPending, isTransitioning }) =>
+                [
+                  isPending ? "pending text-mainwhite" : "",
+                  isActive ? "active text-crimson" : "text-mainwhite",
+                  isTransitioning ? "transitioning text-mainwhite" : "",
+                ].join(" ")
+              }
+            >
+              <p className="sm:text-maindark  duration-100 hover:cursor-pointer hover:text-crimson">
+                contact
+              </p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive, isPending, isTransitioning }) =>
+                [
+                  isPending ? "pending text-mainwhite" : "",
+                  isActive ? "active text-crimson" : "text-mainwhite",
+                  isTransitioning ? "transitioning text-mainwhite" : "",
+                ].join(" ")
+              }
+              to="/gallery"
+            >
+              <p className="sm:text-maindark  duration-100 hover:cursor-pointer  hover:text-crimson">
+                gallery
+              </p>
+            </NavLink>
+          </li>
+        </ul>
         <div className="flex gap-5">
           <a
             href="https://www.linkedin.com/in/samuel-bonghanoy-55283a250/"
-            className="hover:cursor-pointer"
+            className="hover:cursor-pointer sm:hidden"
           >
             <LinkedInIcon
               sx={{
@@ -77,7 +82,7 @@ function Navbar() {
           </a>
           <a
             href="https://github.com/Samuel-Bonghanoy"
-            className="hover:cursor-pointer"
+            className="hover:cursor-pointer sm:hidden"
           >
             <GitHubIcon
               sx={{
