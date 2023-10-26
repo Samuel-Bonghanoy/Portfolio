de6586b34be1cbecddfc569bdc02ea2ef6e7ce4c
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-26 10:10:17 +0800

    feat: edit responsive screen breakpoints in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        sm: "380px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "640px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
       boxShadow: {
         maindark:
           "0 4px 6px -1px rgba(34, 33, 17, 0.1), 0 2px 4px -1px rgba(34, 33, 17, 0.06)",
